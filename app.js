f94f59535975ace6d38c7a84e086e4ea88f07e64
Muhammad Zubair Sadaqat <Zubairsadaqat714@gmail.com>
2022-11-29 14:21:04 +0500

    video 9 completed -  uderstaning templating and diff layouts with same style like css and Jquery

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 var items = ["Food", "Drink" ,'eat'];
+var workitems = [];
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs');
 app.use(express.static("public"))
+
 
 app.get("/", (req,res)=>{
     const today = new Date();
@@ -17,16 +19,37 @@ app.get("/", (req,res)=>{
     };
     var day = today.toLocaleDateString("en-PK", options);
 
-    res.render('list', {KindofDay: day, NewItemlists : items});
+    res.render('list', {listTitle: day, NewItemlists : items});
 });
 
 app.post("/", (req,res)=>{
+    // console.log(req.body)
     let str = req.body.newItem;
-    items.push(str);
-    res.redirect("/");
+    if (req.body.list === "work list")
+    {
+        workitems.push(str);
+        res.redirect("/work")
+    }
+    else {
+        items.push(str);
+        res.redirect("/");
+    }
 
 })
 
+app.get("/work" , (req,res)=>{
+    res.render('list', {listTitle: "work list", NewItemlists : workitems});
+})
+
+app.post("/work" , (req,res)=>{
+    let item = req.body.newItem;
+    workitems.push(item);
+    res.redirect("/work")
+})
+
+app.get("/About", (req,res)=>{
+    res.render("About");
+})
 
 app.listen(3000, ()=>{
     console.log("Server is listing on port : 3000")
