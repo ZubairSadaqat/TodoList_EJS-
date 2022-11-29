8984a05fc11573318c24c2e30d6885d547eeae4b
Muhammad Zubair Sadaqat <Zubairsadaqat714@gmail.com>
2022-11-29 07:38:42 +0500

    video 6 completed - implementing todo list with EJS tags using loop

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+var items = ["Food", "Drink" ,'eat'];
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs');
 app.get("/", (req,res)=>{
-    const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     const today = new Date();
-    const day = days[today.getDay()];
-    // if (today.getDay === 0 || today.getDay === 6 ){
-    //     day = "Weekend";
-    // }else {
-    //     day = "Week Day";
-    // }
-    res.render('list', {KindofDay: day});
+    // const day = days[today.getDay()];
+    var options = {
+        weekday : "long",
+        day: "numeric",
+        month:"long"
+    };
+    var day = today.toLocaleDateString("en-PK", options);
+
+    res.render('list', {KindofDay: day, NewItemlists : items});
 });
+
+app.post("/", (req,res)=>{
+    let str = req.body.newItem;
+    items.push(str);
+    res.redirect("/");
+
+})
+
 
 app.listen(3000, ()=>{
     console.log("Server is listing on port : 3000")
